f3e2aeb97da4b7e1a0b9cd303812ecd0f19bebe2
Egor <hoboman313@gmail.com>
2017-06-03 12:17:45 -0700

    no ads

--- web/config/environment.js
+++ web/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'huegasm',
     podModulePrefix: 'huegasm/pods',
-    environment: environment,
+    environment,
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {

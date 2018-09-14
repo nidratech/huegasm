ca9b033f6ec8bf97a7cd8ce9ef63ced83bdbb736
Egor <hoboman313@gmail.com>
2018-09-14 16:28:34 -0700

    don't fingerprint favicon stuff, adding a og image

--- web/ember-cli-build.js
+++ web/ember-cli-build.js
@@ -7,7 +7,7 @@ const Funnel = require('broccoli-funnel');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint:{
-      exclude: ['logo.png']
+      exclude: ['logo.png', 'android-chrome-192x192.png', 'android-chrome-512x512.png', 'apple-touch-icon.png', 'favicon-16x16.png', 'favicon-32x32.png', 'mstile-150x150.png']
     }
   });
   let extraAssets = new Funnel('bower_components/bootstrap-sass/assets/fonts/bootstrap/', {
